6113210d414f4104fe5b50cf61d3853deb815397
Aida Sow <sowaida@ept.sn>
2024-03-16 22:14:14 +0100

    add two db

--- processus_fournisseur/app/api/provider.py
+++ processus_fournisseur/app/api/provider.py
@@ -11,6 +11,9 @@ router = APIRouter()
 async def save_order(payload):
     return await add_order(payload)
 
+async def add_client(payload):
+    return await add_client(payload)
+
 
 def check_order(order_id: int, background_tasks: BackgroundTasks):
     status = "valide"
@@ -25,6 +28,7 @@ def confirm_order(verif_status):
     print(response.text)
 
 
+
 # add new order
 @router.post("/place_order")
 async def add_order(payload: Order, background_tasks: BackgroundTasks):
@@ -32,8 +36,14 @@ async def add_order(payload: Order, background_tasks: BackgroundTasks):
     response = {
         'message': f"Votre commande numero a été bien reçue et est en cours de traitement",
     }
+    with httpx.Client() as client:
+        response = client.post("client-endponit", json=payload)
     return response
 
+@router.get("/orders")
+async def get_orders():
+    orders = await get_all_orders()
+    return orders
 
 @router.post("/message_rabbit")
 async def send_message(queue, message):#payload: Order, background_tasks: BackgroundTasks):
@@ -41,3 +51,16 @@ async def send_message(queue, message):#payload: Order, background_tasks: Backgr
     sender = await RabbitMQSender().send_message(queue, message)
     return sender
 
+
+@router.post("/add_client")
+async def add_client(payload: Order, background_tasks: BackgroundTasks):
+    background_tasks.add_tasks(add_client, payload)
+    response = {
+        'message': f"Client successfully added!",
+    }
+    return response
+
+@router.get("/clients")
+async def get_clients():
+    clients = await get_all_clients()
+    return clients
