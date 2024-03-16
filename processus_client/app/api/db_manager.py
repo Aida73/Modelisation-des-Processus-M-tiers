c18bd5ff89ec14829377154d5c028cc5ceefbb85
aiMBF <aissasaykou@gmail.com>
2024-03-16 22:19:48 +0100

    Adding db_manager changes

--- processus_client/app/api/db_manager.py
+++ processus_client/app/api/db_manager.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException
 from .models import *
-from .db import orders, database
+from .db import *
 
 
 async def add_order(payload:Order):
@@ -15,3 +15,10 @@ async def get_order(order_id: str):
 async def get_all_orders():
     query = orders.select()
     return await database.fetch_all(query=query)
+
+async def add_devis(payload:Devis):
+    query = devis.insert().values(**payload.model_dump())
+    return await database.execute(query=query)
+
+async def valider_commande(order_id:str, status:str):
+    query = orders.update().where(order_id=order_id)
